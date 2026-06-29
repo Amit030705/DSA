4620b617e44af4d1a6f6673280f47c70bc36c71f
Amit030705 <amit.k.030705@gmail.com>
2026-06-29 12:22:27 +0530

    lower bound

---
+++ Binary_Search/lowerBound.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int lowerBound(vector<int> arr, int n, int x) {
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        // Maybe this is the answer
+        if (arr[mid] >= x) {
+            ans = mid;
+            high = mid - 1;   // Search on the left
+        }
+        else {
+            low = mid + 1;    // Search on the right
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+
+    cout << "Enter the sorted array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int x;
+    cout << "Enter the value to find lower bound: ";
+    cin >> x;
+
+    int index = lowerBound(arr, n, x);
+
+    if (index == n)
+        cout << "Lower bound does not exist." << endl;
+    else
+        cout << "Lower bound index = " << index
+             << "\nLower bound value = " << arr[index] << endl;
+
+    return 0;
+}
